384a9c1c49d41815436af6fe97995d07731df3f7
milosnikic <milos.nikic@tnation.eu>
2022-05-25 12:58:28 +0200

    - Test cases grouped accordingly

--- Program.cs
+++ Program.cs
@@ -9,59 +9,20 @@ namespace HL7Serializer
     {
         static void Main(string[] args)
         {
-            var header = new MessageHeader()
-            {
-                SendingApplication = "MEDFLOW",
-                SendingFacility = "MEDFLOW",
-                ReceivingFacility = "MEDFLOWDCM",
-                ReceivingApplication = "MEDFLOW",
-                MessageType = "ADT^A01",
-                SentDateTime = DateTime.Now
-            };
-
-            var patient = new Patient()
-            {
-                Id = "76040",
-                AssigningAuthority = "MEDFLOW",
-                BirthDate = new ShortDate(new DateTime(1960, 5, 19)),
-                City = "MALIBU BEACH",
-                Firstname = "Daniel",
-                Lastname = "Craig",
-                Gender = 'M',
-                Street = "6548 SECRET WAY",
-            };
-
-            var mergePatient = new Patient()
-            {
-                Id = "123",
-                AssigningAuthority = "PROOPT"
-            };
-
             var serializer = new HLSevenSerializer();
-            Console.WriteLine("------------------Create patient---------------------");
-            Console.WriteLine(serializer.Serialize(header, patient));
 
-            Console.WriteLine("-------------------Merge patients--------------------");
-            Console.WriteLine(serializer.Serialize(header, patient, mergePatient));
+            MessageHeader header;
+            Patient patient, mergePatient;
+            GetHeaderAndPatients(out header, out patient, out mergePatient);
 
-            var responses = new List<KeyValuePair<bool, string>>()
-            {
-                new KeyValuePair<bool, string>(true, @"MSH|^~\&|||SAP i.s.h. med|KAS|20140108130546+0100||ACK^A40^ACK|14654726|P|2.4\n
-                MSA|AA|14654726||||0^Message accepted"),
-                new KeyValuePair<bool, string>(false, @"MSH|^~\&|||MEDFLOW|MEDFLOW|20140108113941+0100||ACK^A01^ACK|0170cd83|P|2.3\n
-                            MSA|AR|0170cd83|Combination of patient id and issuer of patient id is not unique, identified by field 'patientId' with fieldValue 'MEDFLOW/76040'!|||205^Duplicate key identifier"),
-                new KeyValuePair<bool, string>(true, @"MSH|^~\&|||MEDFLOW|MEDFLOW|20140108113857+0100||ACK^A01^ACK|0170cd83|P|2.3\n
-                                    MSA|AA|0170cd83||||0^Message accepted"),
-            };
+            TestCreatePatient(header, patient, serializer);
+            TestMergePatients(header, patient, mergePatient, serializer);
+            TestResponses(serializer);
+            TestCreateOrder(serializer, header);
+        }
 
-            foreach (var response in responses)
-            {
-                Console.WriteLine(string.Format("------------------{0} response---------------------", response.Key ? "Success" : "Failed"));
-                var deserialized = serializer.Deserialize(response.Value);
-                Console.WriteLine(string.Format("Result:\n{0} \n{1}", deserialized.Key, deserialized.Value));
-            }
-
-
+        private static void TestCreateOrder(HLSevenSerializer serializer, MessageHeader header)
+        {
             Console.WriteLine("-------------------Create order--------------------");
             header.MessageType = "ORM^O01";
             var orderPatient = new Patient()
@@ -152,11 +113,11 @@ namespace HL7Serializer
                     {
                         PrimaryNumber = "(216)444-2020",
                         City = "216",
-                        SecondaryNumber  = "4442020"
+                        SecondaryNumber = "4442020"
                     },
                     QuantityAndTiming = new QuantityAndTiming()
                     {
-                        Priority = "R",   
+                        Priority = "R",
                     },
                     Tehnician = new Person()
                     {
@@ -167,6 +128,67 @@ namespace HL7Serializer
                 },
             };
             Console.WriteLine(serializer.Serialize(header, orderPatient, order));
+        }
+
+        private static void GetHeaderAndPatients(out MessageHeader header, out Patient patient, out Patient mergePatient)
+        {
+            header = new MessageHeader()
+            {
+                SendingApplication = "MEDFLOW",
+                SendingFacility = "MEDFLOW",
+                ReceivingFacility = "MEDFLOWDCM",
+                ReceivingApplication = "MEDFLOW",
+                MessageType = "ADT^A01",
+                SentDateTime = DateTime.Now
+            };
+            patient = new Patient()
+            {
+                Id = "76040",
+                AssigningAuthority = "MEDFLOW",
+                BirthDate = new ShortDate(new DateTime(1960, 5, 19)),
+                City = "MALIBU BEACH",
+                Firstname = "Daniel",
+                Lastname = "Craig",
+                Gender = 'M',
+                Street = "6548 SECRET WAY",
+            };
+            mergePatient = new Patient()
+            {
+                Id = "123",
+                AssigningAuthority = "PROOPT"
+            };
+        }
+
+        private static void TestMergePatients(MessageHeader header, Patient patient, Patient mergePatient, HLSevenSerializer serializer)
+        {
+            Console.WriteLine("-------------------Merge patients--------------------");
+            Console.WriteLine(serializer.Serialize(header, patient, mergePatient));
+        }
+
+        private static void TestCreatePatient(MessageHeader header, Patient patient, HLSevenSerializer serializer)
+        {
+            Console.WriteLine("------------------Create patient---------------------");
+            Console.WriteLine(serializer.Serialize(header, patient));
+        }
+
+        private static void TestResponses(HLSevenSerializer serializer)
+        {
+            var responses = new List<KeyValuePair<bool, string>>()
+            {
+                new KeyValuePair<bool, string>(true, @"MSH|^~\&|||SAP i.s.h. med|KAS|20140108130546+0100||ACK^A40^ACK|14654726|P|2.4\n
+                MSA|AA|14654726||||0^Message accepted"),
+                new KeyValuePair<bool, string>(false, @"MSH|^~\&|||MEDFLOW|MEDFLOW|20140108113941+0100||ACK^A01^ACK|0170cd83|P|2.3\n
+                            MSA|AR|0170cd83|Combination of patient id and issuer of patient id is not unique, identified by field 'patientId' with fieldValue 'MEDFLOW/76040'!|||205^Duplicate key identifier"),
+                new KeyValuePair<bool, string>(true, @"MSH|^~\&|||MEDFLOW|MEDFLOW|20140108113857+0100||ACK^A01^ACK|0170cd83|P|2.3\n
+                                    MSA|AA|0170cd83||||0^Message accepted"),
+            };
+
+            foreach (var response in responses)
+            {
+                Console.WriteLine(string.Format("------------------{0} response---------------------", response.Key ? "Success" : "Failed"));
+                var deserialized = serializer.Deserialize(response.Value);
+                Console.WriteLine(string.Format("Result:\n{0} \n{1}", deserialized.Key, deserialized.Value));
+            }
         }
     }
 }
